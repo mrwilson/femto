64398131bff5ff1ce08abaf7297a0fdf0f0574c8
Alex Wilson <gpg@probablyfine.co.uk>
2020-10-10 13:26:48 +0100

    feat: Initial commit

---
+++ src/main/java/uk/co/probablyfine/inject/FemtoInjector.java
@@ -0,0 +1,5 @@
+package uk.co.probablyfine.inject;
+
+public class FemtoInjector {
+
+}
---
+++ src/test/java/uk/co/probablyfine/inject/FemtoInjectorTest.java
@@ -0,0 +1,11 @@
+package uk.co.probablyfine.inject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class FemtoInjectorTest {
+    @Test void testSomeLibraryMethod() {
+        assertTrue(true);
+    }
+}
