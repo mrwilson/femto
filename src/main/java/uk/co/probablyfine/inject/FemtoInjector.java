b62b6576cee837b9726c7d5c98ff1321333e11a6
Alex Wilson <gpg@probablyfine.co.uk>
2020-10-10 13:35:46 +0100

    feat(injector): Return null for unregistered class

--- src/main/java/uk/co/probablyfine/inject/FemtoInjector.java
+++ src/main/java/uk/co/probablyfine/inject/FemtoInjector.java
@@ -2,4 +2,7 @@ package uk.co.probablyfine.inject;
 
 public class FemtoInjector {
 
+    public <T> T get(Class<T> klass) {
+        return null;
+    }
 }
--- src/test/java/uk/co/probablyfine/inject/FemtoInjectorTest.java
+++ src/test/java/uk/co/probablyfine/inject/FemtoInjectorTest.java
@@ -2,10 +2,18 @@ package uk.co.probablyfine.inject;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.nullValue;
 
 class FemtoInjectorTest {
-    @Test void testSomeLibraryMethod() {
-        assertTrue(true);
+
+    @Test
+    void returnsNullForUnboundClass() {
+        var injector = new FemtoInjector();
+        EmptyClass klass = injector.get(EmptyClass.class);
+
+        assertThat(klass, nullValue());
     }
+
+    static class EmptyClass {}
 }
